9e05e25ddd527a47f032a8c32046edb934c3179b
nekogokoro <nekogokoro024@gmail.com>
2020-09-02 16:10:37 +0900

    commit

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -1,6 +1,8 @@
 <template>
+  <el-tabs v-model="activeName">
+  <el-tab-pane label="ToDo" name="toDo">
   <el-table
-    :data="toDos"
+    :data="filter(toDos,false)"
     style="width: 100%">
      <el-table-column
       prop="finished">
@@ -35,15 +37,56 @@
 　　   </template>
     </el-table-column>
   </el-table>
+   </el-tab-pane>
+  <el-tab-pane label="終了したToDo" name="finishedToDo">
+    <el-table
+    :data="filter(toDos,true)"
+    style="width: 100%">
+     <el-table-column
+      prop="finished">
+      <template v-slot="scope">
+        <el-checkbox
+          v-model="scope.row.finished"
+          @change="updateToDo(scope.row.id, scope.row.finished)">
+        </el-checkbox>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="title"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop=""
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="expired_at"
+      label="">
+    </el-table-column>
+    <el-table-column
+　　  width="120">
+　　   <template v-slot="scope">
+   　　  <el-button
+      　　@click="destroyToDo(scope.row.id)"
+         type="danger"
+         icon="el-icon-delete"
+         circle>
+        </el-button>
+　　   </template>
+    </el-table-column>
+  </el-table>
+  </el-tab-pane>
+</el-tabs>
 </template>
 <script>
  import axios from 'axios'
- import {reject} from 'lodash';
+ import {reject, filter} from 'lodash';
  
   export default {
   data() {
     return {
-      toDos: []
+      toDos: [],
+      activeName: 'ToDo'
       }
     },
   created() {
@@ -68,7 +111,10 @@
                 console.log(res)
             }
           })
-        }
+        },
+      filter(toDos, finished) {
+        return filter(toDos, ['finished', finished])
+          }
       }
-    } 
+    }
 </script>
