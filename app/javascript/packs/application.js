8daeacdf4afad0dfe5c8bbb00ab2fdff987e9854
nekogokoro <nekogokoro024@gmail.com>
2020-09-06 01:16:25 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 //require('jquery')
+import '../stylesheets/application'
+import './bootstrap_custom.js'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
